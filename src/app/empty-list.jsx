89af627b24c65b0206b50cacfa20bb90f278fe7f
David <david.geretti@gmail.com>
2016-03-20 22:38:09 +0100

    Added material-ui

---
+++ src/app/empty-list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  
+  render() {
+    return <div>No element!</div>;
+  }
+}
