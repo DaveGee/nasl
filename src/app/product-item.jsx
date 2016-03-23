b3851905bec1b7d12ef6bfa08af1b271fa476fb1
David <david.geretti@gmail.com>
2016-03-23 20:18:43 +0100

    mobile and list item logic

--- src/app/product-item.jsx
+++ src/app/product-item.jsx
@@ -10,6 +10,8 @@ import MenuItem from 'material-ui/lib/menus/menu-item';
 import Unknown from 'material-ui/lib/svg-icons/action/help';
 import Avatar from 'material-ui/lib/avatar';
 
+const BOUGHT = 'boughtSome', NEEDED = 'needed';
+
 export default class ProductItem extends React.Component {
 
   constructor(props) {
@@ -19,30 +21,53 @@ export default class ProductItem extends React.Component {
       action: []
     };
   }
+  
+  justBoughtSome() {
+    return this.state.action.some(a => a === BOUGHT);
+  }
+  
+  needed() {
+    return this.state.action.some(a => a === NEEDED);
+  }
 
   handleChange(event, value) {
-    if(value.length === 2 && this.state.action.some(a => a === 'boughtSome'))
-      this.setState({ action: ['needed']});
+    if(value.length === 2 && this.justBoughtSome())
+      this.setState({ action: [NEEDED]});
     else if(value.length === 2)
-      this.setState({ action : ['boughtSome']});
+      this.setState({ action : [BOUGHT]});
     else   
       this.setState({ action: value });
+  }
+  
+  switchState(event) {
+    if(this.justBoughtSome())
+      this.setState({ action: [] });
+    else if(this.needed())
+      this.setState({ action: [BOUGHT] });
+    else
+      this.setState({ action: [NEEDED] });
   }
 
   render() {
     let lastBuyDate = this.state.product.lastBuy ? 'Dernier achat: ' + this.state.product.lastBuy.format('Do MMM.') : 'Jamais acheté';
 
+    let icon = <MoreVertIcon />;
+    if (this.state.action.some(a => a === NEEDED))
+      icon = <NeededIcon />;
+    else if (this.state.action.some(a => a === BOUGHT))
+      icon = <BoughtIcon />;
+
     let iconButtonElement = (
       <IconButton touch={true}>
-        <MoreVertIcon />
+        {icon}
       </IconButton>
     );
 
     let rightIconMenu = (
       <IconMenu iconButtonElement={iconButtonElement}
-                onChange={this.handleChange.bind(this)}
-                value={this.state.action}
-                multiple={true}>
+          onChange={this.handleChange.bind(this) }
+          value={this.state.action}
+          multiple={true}>
         <MenuItem value="boughtSome" primaryText="J'en ai acheté c'est bon" leftIcon={<BoughtIcon />} />
         <MenuItem value="needed" primaryText="Il en faut absolument" leftIcon={<NeededIcon />} />
       </IconMenu>
@@ -52,6 +77,7 @@ export default class ProductItem extends React.Component {
                      leftAvatar={<Avatar icon={<Unknown />} />}
                      rightIconButton={rightIconMenu}
                      primaryText={this.state.product.name}
-                     secondaryText={lastBuyDate} />;
+                     secondaryText={lastBuyDate}
+                     onTouchTap={this.switchState.bind(this)} />;
   }
 }
